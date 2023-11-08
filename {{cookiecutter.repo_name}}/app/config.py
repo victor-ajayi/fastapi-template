80022d49dbdb7ce4b33547f35241f8f6bba1984a
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-11-08 18:08:29 +0300

    add choice variables for database

--- {{cookiecutter.repo_name}}/app/config.py
+++ {{cookiecutter.repo_name}}/app/config.py
@@ -7,6 +7,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ALGORITHM: str
     POSTGRES_URL: str
+    DB_URL: str
     SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
--- {{cookiecutter.repo_name}}/app/database.py
+++ {{cookiecutter.repo_name}}/app/database.py
@@ -3,14 +3,19 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from .config import settings
 
-{% if cookiecutter.database == "sqlite" %}
+{% if cookiecutter.database == "SQLite" %}
 
 SQLALCHEMY_DATABASE_URL = f"sqlite:///./app.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 {% else %}
 
+{% if cookiecutter.database == "PostgreSQL" %}
 SQLALCHEMY_DATABASE_URL = settings.POSTGRES_URL
+{% else %}
+SQLALCHEMY_DATABASE_URL = settings.DB_URL
+{% endif %}
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 {% endif %}
